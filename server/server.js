6fb6218a576788d9f9e2ff8a95687e955582dc68
wapjude <wapjude@gmail.com>
2017-05-22 19:31:14 +0100

    heroku set up

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   console.log("i am in the server");
-  app.use(express.static('client/build'));
+  //app.use(express.static('client/build'));
 }
 
 routes(router);
